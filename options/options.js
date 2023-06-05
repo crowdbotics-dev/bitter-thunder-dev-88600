f6c2401ebd67db9fd02c6abf43d343966561a43d
Crowdbotics <engineering@crowdbotics.com>
2023-06-05 01:13:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bitter-thunder-dev-88600",
+  url: "https://bitter-thunder-dev-88600.botics.co",
+  api: "https://bitter-thunder-dev-88600.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
